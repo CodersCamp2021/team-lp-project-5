c56ee96b184c0d99463c95b3368190658f889a51
pastepi <p.stepien@livechat.com>
2022-05-07 00:31:44 +0200

    Installed dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>Hello World</div>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
