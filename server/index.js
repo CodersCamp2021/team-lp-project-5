f1e1ded5a243f5dd87691d9d1367751951c63154
Burbinox <hubert.grobelny@interia.pl>
2022-05-09 08:23:15 +0200

    fix backend

--- server/index.js
+++ server/index.js
@@ -1,22 +1,30 @@
-const path = require('path');
-const express = require('express');
+import path from "path";
+import express from "express";
+import { userRouter } from "./routes/user.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+app.use(express.json());
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+// Routers
+app.use("/api/user", userRouter);
+
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
