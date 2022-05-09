5f7ac2ebd466258713a818101bacb4687d596074
Maciek <maciek.duzy.11@gmail.com>
2022-05-09 21:42:13 +0200

    create calendar page

--- client/src/components/shell/Shell.jsx
+++ client/src/components/shell/Shell.jsx
@@ -4,6 +4,21 @@ import { AppShell, Text, Box } from "@mantine/core";
 import { useShellStyles } from "../../hooks/styles/use-shell-styles";
 import Header from "./header/Header";
 import Navbar from "./navbar/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import CalendarPage from "../calendar/CalendarPage";
+
+// const useStyles = createStyles((theme) => ({
+//   shell: {
+//     main: {
+//       [theme.fn.smallerThan("sm")]: {
+//         minHeight: "calc(100vh - 80px)",
+//       },
+//       [theme.fn.largerThan("sm")]: {
+//         minHeight: "100vh",
+//       },
+//     },
+//   },
+// }));
 
 const Shell = () => {
   const [opened, setOpened] = useState(false);
@@ -15,9 +30,11 @@ const Shell = () => {
       navbar={<Navbar opened={opened} />}
       header={<Header opened={opened} setOpened={setOpened} />}
     >
-      <Box>
-        <Text>Content</Text>
-      </Box>
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/statistics" element={<div>Statistics</div>} />
+        <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
     </AppShell>
   );
 };
