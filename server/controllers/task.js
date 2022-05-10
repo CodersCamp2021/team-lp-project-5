713d139abd387470a91d7c305b134f8c2d33f20d
Burbinox <hubert.grobelny@interia.pl>
2022-05-10 18:51:47 +0200

    initial commit

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -7,6 +7,15 @@ export default class TaskController {
       `INSERT INTO tasks (title, description, user_id, priority, status) VALUES ($1, $2, $3, $4, $5);`,
       [body.title, body.description, body.user_id, body.priority, body.status],
     );
-    return { message: "Ticket created" };
+    return { message: "Task created" };
+  };
+
+  static changeTaskStatusOrPriority = async (body) => {
+    const client = await pool.connect();
+    await client.query(
+      `UPDATE tasks SET priority=$1, status=$2 WHERE task_id=$3;`,
+      [body.priority, body.status, body.task_id],
+    );
+    return { message: "Task changed" };
   };
 }
--- server/routes/task.js
+++ server/routes/task.js
@@ -11,4 +11,13 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/changeStatus", async (req, res) => {
+  try {
+    const response = await TaskControler.changeTaskStatusOrPriority(req.body);
+    return res.status(201).json(response);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
 export { router as taskRouter };
