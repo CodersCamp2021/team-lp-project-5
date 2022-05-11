5eaa181e09e1eab75aabfafd6afe8a76ddb96859
Krzysztof Prońko <pronko.krzysztof@gmail.com>
2022-05-11 20:59:06 +0200

    add more button

--- client/src/components/AddTask/AddTask.jsx
+++ client/src/components/AddTask/AddTask.jsx
@@ -8,12 +8,14 @@ import {
   Button,
   Group,
   SimpleGrid,
+  Collapse
 } from "@mantine/core";
 import { DatePicker, TimeInput } from "@mantine/dates";
 import { BsClock, BsCircleFill, BsChevronDown } from "react-icons/bs";
 import { CgClose } from "react-icons/cg";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
+import {useState} from "react";
 
 const useStyles = createStyles((theme) => ({
   container: {
@@ -31,6 +33,9 @@ const useStyles = createStyles((theme) => ({
 const AddTask = () => {
   const { classes } = useStyles();
   let navigate = useNavigate();
+  const [opened, setOpen] = useState(false);
+
+
   return (
     <Container p="md" className={classes.container}>
       <Text align="right">
@@ -112,7 +117,7 @@ const AddTask = () => {
               },
             }}
           ></Button>
-          <Button
+          <Button onClick={() => setOpen((o) => !o)}
             rightIcon={<BsChevronDown size={25} />}
             styles={{
               root: {
@@ -129,178 +134,174 @@ const AddTask = () => {
           >
             MORE
           </Button>
-        </Group>
-      </Box>
-      <Text
-        pt="md"
-        className={classes.titlesNames}
-        sx={(theme) => ({
-          [theme.fn.smallerThan("md")]: {
-            fontSize: 14,
-          },
-        })}
-      >
-        COLLECTIONS
-      </Text>
-      <Group position="left">
-        <Button
+        </Group></Box>
+      <Collapse in={opened}><Text
+          pt="md"
+          className={classes.titlesNames}
           sx={(theme) => ({
             [theme.fn.smallerThan("md")]: {
-              fontSize: 12,
+              fontSize: 14,
             },
           })}
-          radius={15}
-          className={classes.button}
-          styles={{
-            root: {
-              width: 120,
-              backgroundColor: "#00ADB5",
-              color: "#393E46"
-            },
-          }}
         >
-          personal
-        </Button>
-        <Button
-          sx={(theme) => ({
-            [theme.fn.smallerThan("md")]: {
-              fontSize: 12,
-            },
-          })}
-          radius={15}
-          className={classes.button}
-          styles={{
-            root: {
-              width: 120,
-              backgroundColor: "#00ADB5",
-              color: "#393E46"
-            },
-          }}
-        >
-          bussiness
-        </Button>
-        <Button
-          sx={(theme) => ({
-            [theme.fn.smallerThan("md")]: {
-              fontSize: 12,
-            },
-          })}
-          radius={15}
-          className={classes.button}
-          styles={{
-            root: {
-              width: 120,
-              backgroundColor: "#00ADB5",
-              color: "#393E46"
-            },
-          }}
-        >
-          friends
-        </Button>
-        <Button
-          sx={(theme) => ({
-            [theme.fn.smallerThan("md")]: {
-              fontSize: 12,
-            },
-          })}
-          radius={15}
-          className={classes.button}
-          styles={{
-            root: {
-              width: 120,
-              backgroundColor: "#00ADB5",
-              color: "#393E46"
-            },
-          }}
-        >
-          family
-        </Button>
-        <Button
-          sx={(theme) => ({
-            [theme.fn.smallerThan("md")]: {
-              fontSize: 12,
-            },
-          })}
-          radius={15}
-          className={classes.button}
-          styles={{
-            root: {
-              width: 120,
-              backgroundColor: "#00ADB5",
-              color: "#393E46"
-            },
-          }}
-        >
-          hobby
-        </Button>
-      </Group>
-      <Text
-        pt="md"
-        className={classes.titlesNames}
-        sx={(theme) => ({
-          [theme.fn.smallerThan("md")]: {
-            fontSize: 14,
-          },
-        })}
-      >
-        PRIORITY
-      </Text>
-      <Group position="left">
-        <Button
-          sx={(theme) => ({
-            [theme.fn.smallerThan("md")]: {
-              fontSize: 12,
-            },
-          })}
-          radius={15}
-          className={classes.button}
-          styles={{
-            root: {
-              width: 120,
-              backgroundColor: "#E06262",
-              color: "#393E46",
-            },
-          }}
-        >
-          urgent
-        </Button>
-        <Button
-          sx={(theme) => ({
-            [theme.fn.smallerThan("md")]: {
-              fontSize: 12,
-            },
-          })}
-          radius={15}
-          className={classes.button}
-          styles={{
-            root: {
-              width: 120,
-              backgroundColor: "#EEB45E",
-              color: "#393E46",
-            },
-          }}
-        >
-          important
-        </Button>
-        <Button
-          sx={(theme) => ({
-            [theme.fn.smallerThan("md")]: {
-              fontSize: 12,
-            },
-          })}
-          radius={15}
-          className={classes.button}
-          styles={{
-            root: {
-              width: 120,
-              backgroundColor: "#7BD650",
-              color: "#393E46",
-            },
-          }}
-        >
-          time waste
-        </Button>
-      </Group>
+          COLLECTIONS
+        </Text><Group position="left">
+            <Button
+              sx={(theme) => ({
+                [theme.fn.smallerThan("md")]: {
+                  fontSize: 12,
+                },
+              })}
+              radius={15}
+              className={classes.button}
+              styles={{
+                root: {
+                  width: 120,
+                  backgroundColor: "#00ADB5",
+                  color: "#393E46"
+                },
+              }}
+            >
+              personal
+            </Button>
+            <Button
+              sx={(theme) => ({
+                [theme.fn.smallerThan("md")]: {
+                  fontSize: 12,
+                },
+              })}
+              radius={15}
+              className={classes.button}
+              styles={{
+                root: {
+                  width: 120,
+                  backgroundColor: "#00ADB5",
+                  color: "#393E46"
+                },
+              }}
+            >
+              bussiness
+            </Button>
+            <Button
+              sx={(theme) => ({
+                [theme.fn.smallerThan("md")]: {
+                  fontSize: 12,
+                },
+              })}
+              radius={15}
+              className={classes.button}
+              styles={{
+                root: {
+                  width: 120,
+                  backgroundColor: "#00ADB5",
+                  color: "#393E46"
+                },
+              }}
+            >
+              friends
+            </Button>
+            <Button
+              sx={(theme) => ({
+                [theme.fn.smallerThan("md")]: {
+                  fontSize: 12,
+                },
+              })}
+              radius={15}
+              className={classes.button}
+              styles={{
+                root: {
+                  width: 120,
+                  backgroundColor: "#00ADB5",
+                  color: "#393E46"
+                },
+              }}
+            >
+              family
+            </Button>
+            <Button
+              sx={(theme) => ({
+                [theme.fn.smallerThan("md")]: {
+                  fontSize: 12,
+                },
+              })}
+              radius={15}
+              className={classes.button}
+              styles={{
+                root: {
+                  width: 120,
+                  backgroundColor: "#00ADB5",
+                  color: "#393E46"
+                },
+              }}
+            >
+              hobby
+            </Button>
+          </Group><Text
+            pt="md"
+            className={classes.titlesNames}
+            sx={(theme) => ({
+              [theme.fn.smallerThan("md")]: {
+                fontSize: 14,
+              },
+            })}
+          >
+            PRIORITY
+          </Text><Group position="left">
+            <Button
+              sx={(theme) => ({
+                [theme.fn.smallerThan("md")]: {
+                  fontSize: 12,
+                },
+              })}
+              radius={15}
+              className={classes.button}
+              styles={{
+                root: {
+                  width: 120,
+                  backgroundColor: "#E06262",
+                  color: "#393E46",
+                },
+              }}
+            >
+              urgent
+            </Button>
+            <Button
+              sx={(theme) => ({
+                [theme.fn.smallerThan("md")]: {
+                  fontSize: 12,
+                },
+              })}
+              radius={15}
+              className={classes.button}
+              styles={{
+                root: {
+                  width: 120,
+                  backgroundColor: "#EEB45E",
+                  color: "#393E46",
+                },
+              }}
+            >
+              important
+            </Button>
+            <Button
+              sx={(theme) => ({
+                [theme.fn.smallerThan("md")]: {
+                  fontSize: 12,
+                },
+              })}
+              radius={15}
+              className={classes.button}
+              styles={{
+                root: {
+                  width: 120,
+                  backgroundColor: "#7BD650",
+                  color: "#393E46",
+                },
+              }}
+            >
+              time waste
+            </Button>
+          </Group></Collapse>
       <SimpleGrid
         cols={3}
         spacing="xl"
@@ -327,10 +328,12 @@ const AddTask = () => {
             },
           })}
           
-        >
+        > 
           DELETE
         </Button>
-        <Button
+        <Button  onClick={() => {
+            navigate("/");
+          }}
           radius={15}
           className={classes.button}
           styles={{
