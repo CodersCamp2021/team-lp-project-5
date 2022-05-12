3ba6f2ed66c236e69e99bc6b67b14beb752a1bca
Burbinox <hubert.grobelny@interia.pl>
2022-05-12 12:55:45 +0200

    add dotevn config

--- server/db/setup.js
+++ server/db/setup.js
@@ -1,5 +1,6 @@
 import pkg from "pg";
 const { Pool } = pkg;
+import "dotenv/config";
 
 export const pool = new Pool({
   connectionString: `postgresql://postgres:${process.env.DATABASE_PASSWORD}@localhost:5432/coderscrew`,
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { userRouter } from "./routes/user.js";
 import { taskRouter } from "./routes/task.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import "dotenv/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
