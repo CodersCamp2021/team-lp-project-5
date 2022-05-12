8781d18695b4982abf4aaedc5c4e8a1417418f34
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-05-12 22:41:40 +0200

    modified eslint rules | added browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage";
+import Shell from "./components/shell/Shell";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Shell />} />
+          <Route path="/home" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
