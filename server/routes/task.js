00c9cde91a3e14a8f97891735208b53b12b26923
Burbinox <hubert.grobelny@interia.pl>
2022-05-12 12:26:59 +0200

    fix typo

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,10 +1,10 @@
 import express from "express";
-import TaskControler from "../controllers/task.js";
+import TaskController from "../controllers/task.js";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
-    const response = await TaskControler.createTask(req.body);
+    const response = await TaskController.createTask(req.body);
     return res.status(201).json(response);
   } catch (error) {
     return res.status(400).json({ error: error.message });
