580a8c0ce826f5f22c261479038be187907e79e1
patrykbrodziak <patryk.brodziak1@gmail.com>
2022-05-13 21:04:53 +0200

    password validators fix

--- common/validators.js
+++ common/validators.js
@@ -3,13 +3,7 @@ import * as yup from "yup";
 const loginSchema = yup.object({
   body: yup.object({
     email: yup.string().required("Email is required!").email("Invalid email!"),
-    password: yup
-      .string()
-      .required("Password is required!")
-      .matches(
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character",
-      ),
+    password: yup.string().required("Password is required!"),
   }),
 });
 
