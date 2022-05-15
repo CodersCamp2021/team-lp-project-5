2058b90db317cc75b1bcb1e016f29e092e598ab8
Maciek <maciek.duzy.11@gmail.com>
2022-05-15 20:14:15 +0200

    remove path

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/homepage/Homepage";
 import Shell from "./components/shell/Shell";
-import CalendarPage from "./components/calendar/CalendarPage";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Shell />} />
         <Route path="/home" element={<Homepage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
       <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
