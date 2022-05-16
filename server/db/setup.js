641eb566a0d312313c6b9911bc65ad51ef556129
patrykbrodziak <patryk.brodziak1@gmail.com>
2022-05-16 22:53:02 +0200

    fix database config

--- server/db/setup.js
+++ server/db/setup.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: `${process.env.DATABASE_URL}`,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
