a997d9061d06b1b8728b9da56aae40ce0f611cb1
pastepi <pawel.st123@gmail.com>
2022-05-17 19:09:15 +0200

    Added localStorage, API hooks

---
+++ client/src/hooks/api/use-api.js
@@ -0,0 +1,132 @@
+import { useMutation, useQuery, useQueryClient } from "react-query";
+
+export const fetchTasks = async () => {
+  const response = await fetch(
+    process.env.REACT_APP_SERVER_URL + "/user/tasks",
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  return response.json;
+};
+
+export const postTask = async (title, description, priority, status) => {
+  const response = await fetch(process.env.REACT_APP_SERVER_URL + "/task", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      description,
+      priority,
+      status,
+    }),
+  });
+  return response.json();
+};
+
+export const updateTask = async (id, priority, status) => {
+  const response = await fetch(
+    process.env.REACT_APP_SERVER_URL + `/task/${id}/changeStatus`,
+    {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        priority,
+        status,
+      }),
+    },
+  );
+  return response.json();
+};
+
+export const fetchDeleteTask = async (id) => {
+  const response = await fetch(
+    process.env.REACT_APP_SERVER_URL + `/task/${id}`,
+    {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  return response.json();
+};
+
+export const postLabel = async (title) => {
+  const response = await fetch(process.env.REACT_APP_SERVER_URL + "/label", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title }),
+  });
+  return response.json();
+};
+
+export const useApi = () => {
+  const { data: tasks } = useQuery("tasks", fetchTasks, {
+    onSuccess: (data) => console.log(data),
+  });
+  const queryClient = useQueryClient();
+
+  const { mutate: createTask } = useMutation(
+    ({ title, description, priority, status }) =>
+      postTask(title, description, priority, status),
+    {
+      onSuccess: (data) => {
+        console.log(data);
+        queryClient.invalidateQueries("tasks");
+      },
+      onError: (error) => {
+        console.error(error);
+      },
+    },
+  );
+
+  const { mutate: changeTask } = useMutation(
+    ({ id, priority, status }) => updateTask(id, priority, status),
+    {
+      onSuccess: (data) => {
+        console.log(data);
+        queryClient.invalidateQueries("tasks");
+      },
+      onError: (error) => {
+        console.error(error);
+      },
+    },
+  );
+
+  const { mutate: deleteTask } = useMutation((id) => fetchDeleteTask(id), {
+    onSuccess: (data) => {
+      console.log(data);
+      queryClient.invalidateQueries("tasks");
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const { mutate: createLabel } = useMutation((title) => postLabel(title), {
+    onSuccess: (data) => {
+      console.log(data);
+      queryClient.invalidateQueries("labels");
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  return { tasks, createTask, changeTask, deleteTask, createLabel };
+};
---
+++ client/src/hooks/api/use-local-storage.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const TASKS = "tasks";
+const LABELS = "labels";
+
+export const useLocalStorage = () => {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem(TASKS)) || [],
+  );
+
+  const createTask = (newTask) => {
+    const newId = uuidv4();
+    const tasksInStore = JSON.parse(localStorage.getItem(TASKS)) || [];
+    const updatedTasks = [...tasksInStore, { ...newTask, taskId: newId }];
+
+    setTasks(updatedTasks);
+    localStorage.setItem(JSON.stringify(updatedTasks));
+
+    return { message: "Task created" };
+  };
+
+  const changeTask = (id, priority, status) => {
+    const tasksInStore = JSON.parse(localStorage.getItem(TASKS)) || [];
+    const taskToChange = tasksInStore.find((task) => task.id === id);
+
+    const updatedTask = {
+      ...taskToChange,
+      priority: priority || taskToChange.priority,
+      status: status || taskToChange.status,
+    };
+
+    const filteredTasks = tasksInStore.filter((task) => task.taskId === id);
+    const updatedTasks = [...filteredTasks, { ...updatedTask }];
+    setTasks(updatedTasks);
+    localStorage.setItem(JSON.stringify(updatedTasks));
+
+    return { message: "Task changed" };
+  };
+
+  const deleteTask = (id) => {
+    const tasksInStore = JSON.parse(localStorage.getItem(TASKS)) || [];
+    if (tasksInStore.find((task) => task.id === id)) {
+      const updatedTasks = tasksInStore.filter((task) => task.taskId === id);
+      setTasks(updatedTasks);
+      localStorage.setItem(JSON.stringify(updatedTasks));
+
+      return { message: "Task Deleted" };
+    } else {
+      return { message: "No task with that ID" };
+    }
+  };
+
+  const createLabel = (title) => {
+    const newId = uuidv4();
+    const labelsInStore = JSON.parse(localStorage.getItem(LABELS)) || [];
+    const updatedLabels = [...labelsInStore, { labelId: newId, title }];
+    localStorage.setItem(JSON.stringify(updatedLabels));
+
+    return { message: "Label created" };
+  };
+
+  return { tasks, createTask, changeTask, deleteTask, createLabel };
+};
