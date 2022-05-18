b0ff4e29b555bd4b495bdebca91a9b799e946c69
patrykbrodziak <patryk.brodziak1@gmail.com>
2022-05-18 22:43:53 +0200

    add task for more label

--- server/controllers/label.js
+++ server/controllers/label.js
@@ -10,10 +10,12 @@ export default class labelController {
   };
 
   static createTaskLabelRelation = async (req) => {
-    await pool.query(
-      `INSERT INTO tasks_labels_relation (task_id, label_id) VALUES ($1, $2);`,
-      [req.body.taskId, req.body.labelId],
-    );
+    for (const label of req.body.labelId) {
+      await pool.query(
+        `INSERT INTO tasks_labels_relation (task_id, label_id) VALUES ($1, $2);`,
+        [req.body.taskId, label],
+      );
+    }
     return {
       message: `Task with ID: ${req.body.taskId} is now labeled by label with ID: ${req.body.labelId}`,
     };
