9eaea5158824f1881e970a4a13dec17aef89c405
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-05-19 12:35:16 +0200

    Added register modal

--- client/src/components/shell/navbar/NavbarUser.jsx
+++ client/src/components/shell/navbar/NavbarUser.jsx
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Anchor, Avatar, Box, Text } from "@mantine/core";
 
 import { useNavbarStyles } from "../../../hooks/styles/use-navbar-styles";
 import { useMediaQuery } from "@mantine/hooks";
+import SignUpModal from "../../homepage/SignUpModal";
 
 const NavbarUser = () => {
   const { classes } = useNavbarStyles();
+  const [opened, setOpened] = useState(false);
   const isGuest = window.localStorage.getItem("userType") === "guest";
   const isXsScreen = useMediaQuery("(max-width: 576px)");
 
   return (
     <Box sx={{ paddingTop: 10 }}>
       {isGuest ? (
-        <Box sx={{}}>
-          <Text
-            sx={{
-              padding: "15px 40px 10px",
-              maxWidth: "400px",
-              textAlign: "center",
-              fontSize: "20px",
-            }}
-          >
-            You are using app as a guest. If you want to access full
-            functionality, please{" "}
-            <Anchor sx={{ fontSize: "20px" }}>register</Anchor>.
-          </Text>
-        </Box>
+        <>
+          <Box sx={{}}>
+            <Text
+              sx={{
+                padding: "15px 40px 10px",
+                maxWidth: "400px",
+                textAlign: "center",
+                fontSize: "20px",
+              }}
+            >
+              You are using app as a guest. If you want to access full
+              functionality, please{" "}
+              <Anchor onClick={() => setOpened(true)} sx={{ fontSize: "20px" }}>
+                register
+              </Anchor>
+              .
+            </Text>
+          </Box>
+          <SignUpModal opened={opened} setOpened={setOpened} />
+        </>
       ) : (
         <>
           <Box>
